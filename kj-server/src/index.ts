4e49e344cd8d779a3b64e8322ecbdcfe0addfdf6
Keaton Johnson <keatondavidjohnson@gmail.com>
2021-04-14 17:05:18 -0500

    Added GraphQL.

--- kj-server/src/index.ts
+++ kj-server/src/index.ts
@@ -2,6 +2,9 @@ import { MikroORM } from "@mikro-orm/core";
 import express from "express";
 import { __prod__ } from "./consts";
 import mikroOrmConf from "./mikro-orm.config";
+import { ApolloServer } from "apollo-server-express";
+import { buildSchema } from "type-graphql";
+import { HelloResolver } from "./resolvers/hello";
 
 const main = async () => {
   // Postgres is setup to trust my local connection.
@@ -9,6 +12,15 @@ const main = async () => {
   await orm.getMigrator().up();
 
   const app = express();
+
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloResolver],
+      validate: false,
+    }),
+  });
+
+  apolloServer.applyMiddleware({ app });
 
   app.listen(4000, () => {
     console.log("Server started on Port:4000");
